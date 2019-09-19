eb130a0a699b0db648ad8582ed6cb53767fc0df5
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-19 12:07:22 +0200

    chore: setup eslint & semantic-release

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "standard"
+  ]
+};
