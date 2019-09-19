fa6d2bc2e3f6ac81b367921ebe056de1ffee1b41
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-19 13:28:38 +0200

    perf: setup lambda-warmer w/ cloudwatch events to avoid coldstarts

--- handler.ts
+++ handler.ts
@@ -1,7 +1,16 @@
 import { APIGatewayProxyHandler, APIGatewayEvent } from 'aws-lambda'
 import 'source-map-support/register'
+import warmer from 'lambda-warmer'
 
 export const hello: APIGatewayProxyHandler = async (event: APIGatewayEvent, _context) => {
+  // if a warming event
+  if (await warmer(event)) {
+    return {
+      statusCode: 200,
+      body: 'warmed'
+    }
+  }
+  // else proceed with handler logic
   return {
     statusCode: 200,
     body: JSON.stringify({
