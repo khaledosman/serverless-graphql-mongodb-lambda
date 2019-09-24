853fb603a313669a91bbe98fd226fe8edcb8e64e
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-24 20:48:14 +0200

    fix: setup db connection

--- api/graphqlHandler.js
+++ api/graphqlHandler.js
@@ -1,7 +1,12 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
 const { resolvers } = require('../graphql/resolvers')
 const responseCachePlugin = require('apollo-server-plugin-response-cache')
+// const { RedisCache } = require('apollo-server-cache-redis')
+const mongoose = require('mongoose')
 
+let conn = null
+
+const uri = process.env.DEV_MONGO
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
@@ -30,29 +35,46 @@ const server = new ApolloServer({
     debugPrintReports: true,
     schemaTag: process.env.AWS_STAGE
   },
-  //  cache: new RedisCache({
-  //   host: 'redis-server',
+  // cache: new RedisCache({
+  //   host: 'redis-server'
   //   // Options are passed through to the Redis client
   // }),
 
   plugins: [responseCachePlugin()],
-  schemaTag: process.env.AWS_STAGE
-  // context: ({ req }) => {
-  //   // get the user token from the headers
-  //   const token = req.headers.authorization || ''
+  schemaTag: process.env.AWS_STAGE,
+  context: async ({ event, context }) => {
+    // get the user token from the headers
+    // const token = req.headers.authorization || ''
 
-  //   // try to retrieve a user with the token
-  //   const user = getUser(token)
+    // try to retrieve a user with the token
+    // const user = getUser(token)
 
-  //   // add the user to the context
-  //   return { user }
-  // }
-  // persistedQueries: {
-  //  cache: new RedisCache({
-  //   host: 'redis-server',
-  //   // Options are passed through to the Redis client
-  // }),
-  // },
+    // add the user to the context
+    // return { user }
+    context.callbackWaitsForEmptyEventLoop = false
+
+    // Because `conn` is in the global scope, Lambda may retain it between
+    // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+    // This means your Lambda function doesn't have to go through the
+    // potentially expensive process of connecting to MongoDB every time.
+    if (conn === null) {
+      conn = await mongoose.createConnection(uri, {
+        // Buffering means mongoose will queue up operations if it gets
+        // disconnected from MongoDB and send them when it reconnects.
+        // With serverless, better to fail fast if not connected.
+        bufferCommands: false, // Disable mongoose buffering
+        bufferMaxEntries: 0 // and MongoDB driver buffering
+      })
+      // conn.model('Test', new mongoose.Schema({ name: String }))
+    }
+    return { mongoose: conn }
+  },
+  persistedQueries: {
+    // cache: new RedisCache({
+    //   host: 'redis-server'
+    // // Options are passed through to the Redis client
+    // })
+  }
 })
 
 exports.graphqlHandler = server.createHandler({
