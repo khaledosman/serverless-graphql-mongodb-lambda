6deab358c0d4f640780c5170a976bc59a95fb739
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-27 12:31:56 +0200

    fix: use localSchemaFile for engine, different service name per stage

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   service: {
-    endpoint: {
-      url: 'https://h7y51nnuhb.execute-api.eu-central-1.amazonaws.com/dev/graphql'
-    }
-    // // OR
-    // localSchemaFile: './path/to/schema.graphql'
+    localSchemaFile: './graphql/schema.graphql'
   }
 }
